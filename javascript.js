dec3b320da0a37cd3d17de985ccaf795e71f68c9
“alyssabjavier” <alyssabjavier@gmail.com>
2023-05-11 18:09:25 -0500

    Got playRound function to work.

--- javascript.js
+++ javascript.js
@@ -4,45 +4,48 @@ function getComputerChoice(items) {
 }
 
 let items = ['rock', 'paper', 'scissors'];
-
-let computerSelection = getComputerChoice(items);
-console.log(computerSelection);
-
-
-//get player choice
-let playerSelection = prompt("Rock, paper, or scissors?");
-
-
-//game
-
 let roundWinner = '';
 let playerScore = 0;
 let computerScore = 0;
 
+//game
+function playRound() {
+//get computer choice
+    let computerSelection = getComputerChoice(items);
+        console.log(computerSelection);
+//get player choice
+    let playerSelection = prompt("Rock, paper, or scissors?");
 
-if (playerSelection.toLowerCase() === computerSelection) {
-   roundWinner = 'tie';
-    alert('No winner!');
-    console.log(playerScore);
-    console.log(roundWinner);
+    if (playerSelection.toLowerCase() === computerSelection) {
+        roundWinner = 'tie';
+        alert('No winner!');
 } 
 
-if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper' ||
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper' ||
     playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors' ||
     playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-    roundWinner = 'computer';
-    computerScore++;
-    console.log(computerScore);
-    console.log(roundWinner);
-    alert('You lose!')
+        roundWinner = 'computer';
+        computerScore++;
+        alert('You lose!')
 } 
-if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors' || 
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors' || 
     playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock' ||
     playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
         roundWinner = 'player';
         playerScore++;
-        console.log(playerScore);
-        console.log(roundWinner);
         alert('You win!')      
     }
+}
+
+playRound();
+console.log(playerScore);
+console.log(computerScore);
+
+playRound();
+console.log(playerScore);
+console.log(computerScore);
+
+playRound();
+console.log(playerScore);
+console.log(computerScore);
 
