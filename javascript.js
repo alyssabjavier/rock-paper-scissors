5237c5527f679de19158fa61fd967c0c7696a5c7
“alyssabjavier” <alyssabjavier@gmail.com>
2023-05-11 18:23:15 -0500

    Game is working properly

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ let roundWinner = '';
 let playerScore = 0;
 let computerScore = 0;
 
-//game
+//1 round of game
 function playRound() {
 //get computer choice
     let computerSelection = getComputerChoice(items);
@@ -16,27 +16,33 @@ function playRound() {
 //get player choice
     let playerSelection = prompt("Rock, paper, or scissors?");
 
+//if tie
     if (playerSelection.toLowerCase() === computerSelection) {
         roundWinner = 'tie';
-        alert('No winner!');
+        alert('No winner this round!');
 } 
 
+//if player loses
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper' ||
     playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors' ||
     playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
         roundWinner = 'computer';
         computerScore++;
-        alert('You lose!')
+        alert('You lose this round!')
 } 
+
+//if player wins
     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors' || 
     playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock' ||
     playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
         roundWinner = 'player';
         playerScore++;
-        alert('You win!')      
+        alert('You win this round!')      
     }
 }
 
+//play 5 rounds and keep score and declare winner
+function game() {
 playRound();
 console.log(playerScore);
 console.log(computerScore);
@@ -49,3 +55,25 @@ playRound();
 console.log(playerScore);
 console.log(computerScore);
 
+playRound();
+console.log(playerScore);
+console.log(computerScore);
+
+playRound();
+console.log(playerScore);
+console.log(computerScore);
+
+if (playerScore > computerScore) {
+    alert(playerScore + ' to ' + computerScore + ', ' + 'You won the game!')
+}
+
+if (computerScore > playerScore) {
+    alert(playerScore + ' to ' + computerScore + ', ' + 'You lost the game!')
+}
+
+if (playerScore === computerScore) (
+    alert(playerScore + ' to ' + computerScore + ', ' + 'The game is a tie!')
+)
+}
+
+game();
