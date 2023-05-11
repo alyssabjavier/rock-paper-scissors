afde040ecb4e2b27b15568699ba59a3e947c6edd
“alyssabjavier” <alyssabjavier@gmail.com>
2023-05-10 19:27:03 -0500

    Game playable for one round

--- javascript.js
+++ javascript.js
@@ -1,9 +1,48 @@
-function random_item(items)
-{
-  
-return items[Math.floor(Math.random()*items.length)];
-     
+//make computer choose rock, paper, or scissors
+function getComputerChoice(items) {
+  return items[Math.floor(Math.random()*items.length)];   
 }
 
-var items = ['rock', 'paper', 'scissors'];
-console.log(random_item(items));
+let items = ['rock', 'paper', 'scissors'];
+
+let computerSelection = getComputerChoice(items);
+console.log(computerSelection);
+
+
+//get player choice
+let playerSelection = prompt("Rock, paper, or scissors?");
+
+
+//game
+
+let roundWinner = '';
+let playerScore = 0;
+let computerScore = 0;
+
+
+if (playerSelection.toLowerCase() === computerSelection) {
+   roundWinner = 'tie';
+    alert('No winner!');
+    console.log(playerScore);
+    console.log(roundWinner);
+} 
+
+if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper' ||
+    playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors' ||
+    playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+    roundWinner = 'computer';
+    computerScore++;
+    console.log(computerScore);
+    console.log(roundWinner);
+    alert('You lose!')
+} 
+if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors' || 
+    playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock' ||
+    playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        roundWinner = 'player';
+        playerScore++;
+        console.log(playerScore);
+        console.log(roundWinner);
+        alert('You win!')      
+    }
+
